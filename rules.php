775a81549bc3be0dffc69716d62001f33223e28b
Ansidion <14023216@uhi.ac.uk>
2023-02-23 17:16:14 +0000

    adding rules and changing link on header page

--- rules.php
+++ rules.php
@@ -1,3 +1,36 @@
 <?php require 'header.php' ?>
 
+<h1 class="rulesTitle">RULES</h1>
+
+<p class="rules">
+    1. Be respectful: Treat others with respect and be polite in your interactions. Personal attacks, insults, and derogatory language will not be tolerated.
+    <br>
+    <br>
+    2. Stay on topic: Make sure your posts are related to the game or topic being discussed in the forum. Avoid going off-topic or derailing threads.
+    <br>
+    <br>
+    3. No spamming or self-promotion: Do not spam the forum with repetitive or irrelevant messages. Self-promotion is only allowed in designated areas or if it is relevant to the discussion.
+    <br>
+    <br>
+    4. Do not share pirated content: Sharing or requesting illegal copies of games or copyrighted material is strictly prohibited.
+    <br>
+    <br>
+    5. No cheating or hacking discussion: Discussion of cheating or hacking in games is not allowed. This includes sharing of hacks, cheats, or exploits.
+    <br>
+    <br>
+    6. Keep it clean: Avoid using vulgar language, posting inappropriate content, or linking to inappropriate websites.
+    <br>
+    <br>
+    7. No trolling: Do not intentionally post provocative or misleading messages with the intention of causing arguments or controversy.
+    <br>
+    <br>
+    8. Follow the rules: Failure to follow the forum rules may result in warnings, suspension, or even a ban from the website.
+    <br>
+    <br>
+    9. Report any issues: If you encounter any issues or rule violations, please report them to the moderators or site administrators.
+    <br>
+    <br>
+    Remember, the forum is a community where people come to discuss their favorite games and share information. By following these rules, you can help make it a friendly and welcoming place for all users.
+</p>
+
 <?php require 'footer.php' ?>
