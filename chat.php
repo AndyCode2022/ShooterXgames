3bf084ba8131b79e0ad28ccb51b01df19447b598
Ansidion <14023216@uhi.ac.uk>
2023-02-24 17:35:42 +0000

    latest changes

--- chat.php
+++ chat.php
@@ -1,3 +1,24 @@
 <?php require 'header.php' ?>
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-2">
+            Column 1
+        </div>
+        <div class="col-md-2">
+            Column 2
+        </div>
+        <div class="col-md-2">
+            Column 3
+        </div>
+        <div class="col-md-2">
+            Column 4
+        </div>
+        <div class="col-md-2">
+            Column 5
+        </div>
+    </div>
+</div>
+
+
 <?php require 'footer.php' ?>
--- index.php
+++ index.php
@@ -2,4 +2,17 @@
 
 <div id="demo"></div>
 
+<!-- Clock that continuously updates using JavaScript -->
+<div class="container-fluid bg-dark text-light py-5">
+    <div class="container">
+        <h1 class="text-center mb-5">Real Time Clock</h1>
+        <div class="row">
+            <div class="col-12 text-center">
+                <p id="clock" class="fs-1"></p>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="app.js"></script>
+
 <?php require 'footer.php' ?>
