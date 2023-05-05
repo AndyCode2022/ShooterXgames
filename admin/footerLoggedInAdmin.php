213fbf40ce14df7cafd32fdbed7c7f3c70cc350e
Ansidion <14023216@uhi.ac.uk>
2023-05-05 18:09:02 +0100

    changes

--- admin/footerLoggedInAdmin.php
+++ admin/footerLoggedInAdmin.php
@@ -1,54 +1,40 @@
 <!-- footer top section -->
-<footer class="bg-light text-center text-lg-start footer">
-    <div class="container p-4">
-        <div class="row">
-            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Footer Content</h5>
-                <p>
-                    Welcome to ShooterXgames! Home of one of the best
-                    first person pc games shooter forums on the internet.
-                </p>
-            </div>
-            <!-- footer links -->
-            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Links</h5>
+<footer class="footer">
+    <!-- <div class="col-lg-6 col-md-12 mb-4 mb-md-0"> -->
+    <h5>Footer Content</h5>
+    <p>
+        Welcome to ShooterXgames! Home of one of the best
+        first person pc games shooter forums on the internet.
+    </p>
+    <!-- footer links -->
+    <h5>Links</h5>
 
-                <ul class="list-unstyled mb-0">
-                    <li>
-                        <a href="indexAdmin.php" class="text-dark">Home</a>
-                    </li>
-                    <li>
-                        <a href="rulesAdmin.php" class="text-dark">Rules</a>
-                    </li>
-                    <li>
-                        <a href="discussionsAdmin.php" class="text-dark">Discussions</a>
-                    </li>
-                    <li>
-                        <a href="helpAdmin.php" class="text-dark">Help</a>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 class="text-uppercase mb-0">Links</h5>
-
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="contactAdmin.php" class="text-dark">Contact</a>
-                    </li>
-                    <li>
-                        <a href="updateUserAdmin.php" class="text-dark">Change Your Details</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+    <div class="">
+        <ul class="list-unstyled mb-0">
+            <li>
+                <a href="indexAdmin.php" class="text-dark">Home</a>
+            </li>
+            <li>
+                <a href="rulesAdmin.php" class="text-dark">Rules</a>
+            </li>
+            <li>
+                <a href="includes/adminAuthenticate.php">Discussions</a>
+            </li>
+            <li>
+                <a href="helpAdmin.php" class="text-dark">Help</a>
+            </li>
+            <li>
+                <a href="contactAdmin.php" class="text-dark">Contact</a>
+            </li>
+        </ul>
     </div>
-    <!-- Change the colour of the footer background -->
     <!-- Bottom footer settings -->
-    <div class="text-center p-3 footer" style="background-color: rgba(255, 0, 0);">
+    <div class="text-center p-3 footer">
         <p class="theatre">© 2023 ShooterXgames</p>
     </div>
 </footer>
 </body>
+
+</html>
 
 </html>
--- footerLoggedIn.php
+++ footerLoggedIn.php
@@ -1,56 +1,44 @@
 <!-- footer top section -->
-<div class="footerCSS">
-<footer class="bg-light text-center text-lg-start footer">
-    <div class="container p-4">
-        <div class="row">
-            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Footer Content</h5>
-                <p>
-                    Welcome to ShooterXgames! Home of one of the best
-                    first person pc games shooter forums on the internet.
-                </p>
-            </div>
-            <!-- footer links -->
-            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Links</h5>
+<footer class="footer">
+    <!-- <div class="col-lg-6 col-md-12 mb-4 mb-md-0"> -->
+    <h5>Footer Content</h5>
+    <p>
+        Welcome to ShooterXgames! Home of one of the best
+        first person pc games shooter forums on the internet.
+    </p>
+    <!-- footer links -->
+    <h5>Links</h5>
 
-                <ul class="list-unstyled mb-0">
-                    <li>
-                        <a href="index.php" class="text-dark">Home</a>
-                    </li>
-                    <li>
-                        <a href="rules.php" class="text-dark">Rules</a>
-                    </li>
-                    <li>
-                        <a href="includes/adminAuthenticate.php" class="text-dark">Discussions</a>
-                    </li>
-                    <li>
-                        <a href="help.php" class="text-dark">Help</a>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 class="text-uppercase mb-0">Links</h5>
-
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="contact.php" class="text-dark">Contact</a>
-                    </li>
-                    <li>
-                        <a href="updateUser.php" class="text-dark">Change Your Details</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+    <div class="">
+        <ul class="list-unstyled mb-0">
+            <li>
+                <a href="index.php" class="text-dark">Home</a>
+            </li>
+            <li>
+                <a href="rules.php" class="text-dark">Rules</a>
+            </li>
+            <li>
+                <a href="includes/adminAuthenticate.php">Discussions</a>
+            </li>
+            <li>
+                <a href="help.php" class="text-dark">Help</a>
+            </li>
+            <li>
+                <a href="contact.php" class="text-dark">Contact</a>
+            </li>
+            <li>
+                <a href="Register.php" class="text-dark">Register</a>
+            </li>
+            <li>
+                <a href="login.php" class="text-dark">Login</a>
+            </li>
+        </ul>
     </div>
-    <!-- Customisation for the theatre back colour -->
     <!-- Bottom footer settings -->
-    <div class="text-center p-3 footer" style="background-color: rgba(255, 0, 0);">
+    <div class="text-center p-3 footer">
         <p class="theatre">© 2023 ShooterXgames</p>
     </div>
 </footer>
-</div>
 </body>
 
 </html>
